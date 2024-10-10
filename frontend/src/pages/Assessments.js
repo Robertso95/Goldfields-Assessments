144796faf14e52a50b0feeeb1d203d3f3b717602
Samuel-Aut <frantickillaace@gmail.com>
2024-10-11 07:02:07 +1300

    Developing Assessment page UI

--- frontend/src/pages/Assessments.js
+++ frontend/src/pages/Assessments.js
@@ -1,7 +1,19 @@
 // Assessments.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Carousel } from 'antd';
+import { Divider } from 'antd';
 import '../assessments.css';
+
+const contentStyle = {
+  margin: 0,
+  height: '200px',
+  color: '#fff',
+  lineHeight: '200px',
+  textAlign: 'center',
+  background: '#007bff',
+
+};
 
 const Assessments = () => {
   return (
@@ -16,19 +28,56 @@ const Assessments = () => {
         </Link>
       </div>
       <div className="main-content">
-        <h1>Assessments</h1>
-        <div className="dummy-data">
-          <p>Hello, M. Smith</p>
-          <p>Goldfields School</p>
-          <hr className="divider" />
+      <div class="content-container">
+  <div class="dummy-data">
+    <p>Hello, M. Smith</p>
+    <p>Goldfields School</p>
+  </div>
+  <hr class="divider" />
+</div>
+        <div className="boxes-container">
+          <div className="boxes">
+            <div className="box">
+              <Carousel arrows infinite={false}>
+                <div>
+                  <h3 style={contentStyle}>Class 1</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>Class 2</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>Class 3</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>Class 4</h3>
+                </div>
+              </Carousel>
+            </div>
+            <div className="box">
+              <Carousel arrows infinite={false}>
+                <div>
+                  <h3 style={contentStyle}>Assignment 1</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>Assignment 2</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>Assignment 3</h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>Assignment 4</h3>
+                </div>
+              </Carousel>
+            </div>
+          </div>
         </div>
-        <div className="boxes">
-          <div className="box">Class</div>
-          <div className="box">Assignments</div>
-        </div>
+        <div class="content-container">
         <h2 className="students-title">Students</h2>
-        <div className="massive-box"></div>
+        <div className="massive-box-container">
+          <div className="massive-box"></div>
+        </div>
       </div>
+    </div>
     </div>
   );
 };
