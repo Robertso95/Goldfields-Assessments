182ab136484a28b7453a893fced458eb03f83a1d
Samuel-Aut <frantickillaace@gmail.com>
2024-10-14 06:49:57 +1300

    Created assignment Minor fix

--- frontend/src/pages/Assessments.js
+++ frontend/src/pages/Assessments.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Carousel } from 'antd';
-import { Divider } from 'antd';
 import '../assessments.css';
 
 const contentStyle = {
