5272a1acaf8d8a80fce46cb57b21e7a073af1d6e
Samuel-Aut <frantickillaace@gmail.com>
2025-03-24 19:33:18 +1300

    Reapply "quick fix to create assignment page"
    This reverts commit 8e436cd3fcec1ac6ed60331e11fa1d86ad1f0377.

--- backend/controllers/assignmentController.js
+++ backend/controllers/assignmentController.js
@@ -61,6 +61,16 @@ const getTags = async (req, res) => {
   }
 };
 
+const createAssignmentImage = async (req, res) => {
+  const image = new HomeImage({
+    imageUrl: req.body.imageUrl
+  });
+  try {
+    const newImage = await image.save();
+    res.status(201).json(newImage);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+}
 
-
-module.exports = { createAssignment, getAssignments, getSubjects, getAssignmentsType, getTags };
+module.exports = { createAssignment, getAssignments, getSubjects, getAssignmentsType, getTags, createAssignmentImage };
