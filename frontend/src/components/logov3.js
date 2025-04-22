bb24255ea5ede19e8b386aa8d01bcb6b386079ca
Samuel-Aut <frantickillaace@gmail.com>
2025-04-22 15:21:25 +1200

    Tweaking UI
    Added
    - Real Time Date
    - And current hour and returns the appropriate greeting
    - Logo above divder

---
+++ frontend/src/components/logov3.js
@@ -0,0 +1,10 @@
+
+const Logo = () => {
+  return (
+      <div className="logo-container">
+          <img width="110" height="153" src="goldfieldslogo.png" alt=""/>
+      </div>
+  )
+}
+
+export default Logo 
